3f318d5ab8ed60c930c1cd3dc57946905ea5e5e8
Dennis Hodges <fermentationist@gmail.com>
2023-07-13 22:10:53 -0400

    Changed getCompletion to use createChatCompletion api with messages. Replaced getMarkdownPrompt with getSystemPrompt (containing markdown instructions) and moved from index to openAi.

--- src/openAi.ts
+++ src/openAi.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
+import { get } from "http";
 import {
   Configuration,
   CreateModerationResponseResultsInner,
   OpenAIApi,
+  ChatCompletionRequestMessageRoleEnum,
 } from "openai";
 
 export const DEFAULT_POLICED_CATEGORIES = [
@@ -14,6 +16,8 @@ export const DEFAULT_POLICED_CATEGORIES = [
   // "violence",
   // "violence/graphic",
 ];
+
+const DEFAULT_MODEL = "gpt-3.5-turbo-0613";
 
 // initialize OpenAI API
 let configuration = new Configuration({
@@ -58,6 +62,24 @@ export async function failsModeration(
   return false;
 }
 
+function getSystemPrompt(frontMatterToGenerate: string[] = []) {
+  const frontMatterToGenerateCopy = [...frontMatterToGenerate];
+  const lastKey = frontMatterToGenerateCopy.pop();
+  // if there is only one key, we don't need to use the word "and"
+  const frontMatterDescription = frontMatterToGenerateCopy.length
+    ? `${frontMatterToGenerateCopy.join(", ")}${
+        lastKey ? ` and ${lastKey}` : ""
+      }`
+    : `${lastKey ?? ""}`;
+  // if there is no front matter to generate, don't include the front matter part of the prompt
+  const prompt = `The assistant writes blog posts in markdown format. The assistant creates markdown files that include ${
+    frontMatterDescription
+      ? `front matter with ${frontMatterDescription}, and `
+      : ``
+  }content that uses a single main heading and section headings, lists and code blocks (only when the prompt explicitly mentions code) where appropriate, about whatever the subject of the user prompt is.`;
+  return prompt;
+}
+
 // getCompletion returns a promise that resolves to the response from the OpenAI API createCompletion endpoint
 export async function getCompletion(
   prompt: string,
@@ -66,11 +88,13 @@ export async function getCompletion(
     maxTokens,
     model,
     apiKey,
+    frontMatterToGenerate,
   }: {
     temperature?: number;
     maxTokens?: number;
     model?: string;
     apiKey?: string;
+    frontMatterToGenerate?: string[];
   } = {}
 ) {
   if (apiKey) {
@@ -81,20 +105,32 @@ export async function getCompletion(
     openai = new OpenAIApi(configuration);
   }
   try {
+    const messages = [
+      {
+        role: "system" as ChatCompletionRequestMessageRoleEnum,
+        content: getSystemPrompt(frontMatterToGenerate),
+      },
+      {
+        role: "user" as ChatCompletionRequestMessageRoleEnum,
+        content: prompt,
+      },
+    ];
     console.log("Getting completion from OpenAI API...");
     const wordCount = prompt.split(/[\s,.-]/).length;
     const estimatedPromptTokens = Math.round(wordCount * 1.5);
     const difference = 2048 - estimatedPromptTokens;
     performance.mark("start");
-    const response = await openai.createCompletion({
-      model: model ?? "text-davinci-003",
-      prompt,
+    const response = await openai.createChatCompletion({
+      model: model ?? DEFAULT_MODEL,
+      messages,
       max_tokens: maxTokens ?? difference,
       temperature,
     });
     console.log("\nGPT model used:", response?.data?.model);
     console.log("Total tokens:", response?.data?.usage?.total_tokens);
-    return response?.data?.choices && response.data.choices[0].text;
+    return (
+      response?.data?.choices && response.data.choices[0]?.message?.content
+    );
   } catch (error) {
     console.log("Error getting completion from OpenAI API:");
     console.error(error);
